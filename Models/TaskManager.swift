10ded95c61d5e17cea646f83322bdd3bfa6d13de
vladislav <107300120+homelless@users.noreply.github.com>
2025-05-16 16:29:24 +0300

    Создание пользовательского интерфейса

--- Models/TaskManager.swift
+++ Models/TaskManager.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+
+class TaskManager {
+    
+    private var tasks: [Task] = []
+    
+    func addTask(_ task: Task) {}
+    func updateTask(_ task: Task) {}
+    func deleteTask(withId id: UUID) {}
+    func getAllTasks() -> [Task] {}
+    func getTasks(filter: Filter) -> [Task] {}
+    
+    struct Filter {
+        var category: String?
+        var priority: Task.Priority?
+        var isCompleted: Bool?
+        
+    }
+    
+    
+}
